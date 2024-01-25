2a8db21a5d05d31244fe15baa38963b1e37d0c3b
UltraEdit-Clone <codngwthubaid@gmail.com>
2024-01-25 12:37:43 +0530

    Now in project songs are playing and the timeduration and currenttie mentioning are also workings correctly

--- Spotify Script.js
+++ Spotify Script.js
@@ -1,6 +1,20 @@
 console.log('Ready');
-
 let currentSong = new Audio;   //global variable for music 
+
+function SecondsToMinuteSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "invalid input"
+    }
+    else {
+        let Minutes = Math.floor(seconds / 60);
+        let Seconds = Math.floor(seconds % 60)
+        let FormattedMinutes = String(Minutes).padStart(2, '0')
+        let FormattedSeconds = String(Seconds).padStart(2, '0')
+
+        return `${FormattedMinutes}:${FormattedSeconds}`
+    }
+}
+
 async function getSongs() {
     let localAPI = await fetch("http://127.0.0.1:3000/Songs/")
     let response = await localAPI.text()
@@ -21,11 +35,15 @@ async function getSongs() {
 
 // play the music when the user on the icon
 const playmusic = (track) => {
-    // let audio = new Audio("/songs/" + track)
     currentSong.src = "/songs/" + track
     currentSong.play()
-    Play.src = "paused.svg"
+    Play.src = "Svg's/paused.svg"
+    document.querySelector(".songInfo").innerHTML = track
+    document.querySelector(".songTime").innerHTML = "00:00 / 00:00"
+
+
 }
+
 
 
 async function main() {
@@ -36,14 +54,14 @@ async function main() {
     // Get all the songs in the playlist
     let songsUL = document.querySelector(".songs").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songsUL.innerHTML = songsUL.innerHTML + `<li><img src="music.svg" alt="">
+        songsUL.innerHTML = songsUL.innerHTML + `<li><img src="Svg's/music.svg" alt="">
                                                 <div class="info flex">
                                                    <div>${song.replaceAll("%20", " ")}</div>
                                                    <div>Ubaid</div>
                                                 </div>
                                                 <div class="playNow flex justify-content align-item">
                                                 <span>PLay Now</span>
-                                                <img id="Play src="play.svg" alt="play-Img"></div>
+                                                <img src="Svg's/play.svg" alt="play-Img"></div>
                                                 </li>`
     }
 
@@ -57,22 +75,27 @@ async function main() {
 
     })
 
-
     // Attach an event lostener for play the music previous the music and get next music
     // pasued the music and play the music
-    Play.addEventListener("click", ()=>{
-        if(currentSong.paused){
+    Play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play()
-            Play.svg = "paused.svg"
+            Play.src = "Svg's/paused.svg"
         }
-        else{
+        else {
             currentSong.pause()
-            Play.svg = "play.svg"
-
+            Play.src = "Svg's/Play.svg"
         }
     })
 
-   
+    //Listen for time update event
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songTime").innerHTML = 
+        `${SecondsToMinuteSeconds(currentSong.currentTime)}:${SecondsToMinuteSeconds(currentSong.duration)}`
+
+    })
+
 }
 
 main()
