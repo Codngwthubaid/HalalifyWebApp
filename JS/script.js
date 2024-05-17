02fcc1f0c1a1461189e3194f0e0227595a4660ea
Codngwthubaid <codngwthubaid@gmail.com>
2024-05-17 20:42:22 +0530

    Still Working on js and the functionality of the player

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,10 @@
 console.log("Getting Started !!!");
+let currentSong = new Audio
+let play = document.getElementById("playBtn")
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text()
-    console.log(response);
 
     let div = document.createElement("div")
     div.innerHTML = response
@@ -13,19 +14,60 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".m4a")) {
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
-async function main() {
-    // list of all the songs
-    let songs = await getSongs()
-    console.log(songs);
 
-    // play first song
-    var audio = new Audio(songs[0]);
-    audio.play();
+const playMusic = (music) => {
+    currentSong.src = "/songs/" + music
+    currentSong.play()
+    play.src = "Svg's/paused.svg"
 }
 
+
+
+async function main() {
+
+
+    // list of all the songs
+    let songs = await getSongs()
+    // console.log(songs);
+
+    // Show all the songs in the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML +=
+            `<li>
+            <img src="Svg's/music.svg" alt="Music">
+                <div class="info">
+                    <div>${song.replaceAll("%20", " ")}</div>
+                    <div>Ubaid</div>
+                </div>
+            <div>Play Now</div>
+            <img class="invert" style="width: 25px;" src="Svg's/librarayPlayBtn.svg" alt="playBtn">
+        </li>`
+
+        // Add eventlister to each song
+        document.querySelectorAll(".songList li").forEach(element => {
+            element.addEventListener("click", (e) => {
+                console.log(element.querySelector(".info").firstElementChild.innerHTML);
+                playMusic(element.querySelector(".info").firstElementChild.innerHTML.trim())
+            })
+        });
+
+        //Add an eventlistner for play pause
+        play.addEventListener("click", (e) => {
+            if (currentSong.paused) {
+                currentSong.play(e)
+                play.src = "Svg's/paused.svg"
+            } else {
+                currentSong.pause(e)
+                play.src = "Svg's/play.svg"
+            }
+        })
+
+    }
+}
 main()
